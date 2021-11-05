6fb3a58d596a8d5a98a61cac32e4ab843bc17119
padmini v <padmini.v@perfios.com>
2021-11-06 01:56:37 +0530

    MODIFIED: Ability to throw user friendly errors if contactId isn't found

--- src/main/java/com/bridgelabz/service/AddressBookService.java
+++ src/main/java/com/bridgelabz/service/AddressBookService.java
@@ -4,28 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import com.bridgelabz.dto.ContactDTO;
+import com.bridgelabz.exception.AddressBookException;
 import com.bridgelabz.model.ContactData;
 
 @Service
 public class AddressBookService implements IAddressBookService 
 {
-	List<ContactData> contactList=new ArrayList<>();
+	List<ContactData> contactList = new ArrayList<>();
+
 	@Override
 	public List<ContactData> getContact() 
 	{
-        return contactList;
+		return contactList;
 	}
 
 	@Override
 	public ContactData getContactById(int contactId) 
 	{
-	        return contactList.get(contactId-1);
+		return contactList.stream().filter(contact -> contact.getContactId() == contactId).findFirst()
+				.orElseThrow(() -> new AddressBookException("Contact not found!"));
 	}
 
 	@Override
 	public ContactData createContact(ContactDTO contactDTO) 
 	{
-		ContactData contactData=new ContactData(contactList.size()+1,contactDTO);
+		ContactData contactData = new ContactData(contactList.size()+1, contactDTO);
 		contactList.add(contactData);
 		return contactData;
 	}
@@ -33,17 +36,20 @@ public class AddressBookService implements IAddressBookService
 	@Override
 	public ContactData updateContact(int contactId, ContactDTO contactDTO) 
 	{
-		contactList.remove(contactId-1);
-		ContactData contactData=new ContactData(contactId,contactDTO);
-		contactList.add(contactData);
+		contactList.stream().filter(contact -> contact.getContactId() == contactId).findFirst()
+				.orElseThrow(() -> new AddressBookException("Contact not found!"));
+		contactList.remove(contactId - 1);
+		ContactData contactData = new ContactData(contactId, contactDTO);
+		contactList.add(contactId - 1, contactData);
 		return contactData;
 	}
 
 	@Override
 	public void deleteContact(int contactId) 
 	{
-		contactList.remove(contactId-1);
-		
+		contactList.stream().filter(contact -> contact.getContactId() == contactId).findFirst()
+				.orElseThrow(() -> new AddressBookException("Contact not found!"));
+		contactList.remove(contactId - 1);
 	}
 
 }
