72a334aea04481019da00e5f2f84a823a26e37d4
padmini v <padmini.v@perfios.com>
2021-11-07 22:42:33 +0530

    ADDED: Annotations to create table out of ContactData class

--- src/main/java/com/bridgelabz/model/ContactData.java
+++ src/main/java/com/bridgelabz/model/ContactData.java
@@ -2,17 +2,41 @@ package com.bridgelabz.model;
 
 import com.bridgelabz.dto.ContactDTO;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
+@Entity
+@Table(name = "contact")
 public @Data class ContactData 
 {
+	@Id
+	@Column(name = "contact_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int contactId;
+
+	@Column(name = "first_name")
 	private String firstName;
+
+	@Column(name = "last_name")
 	private String lastName;
+
+	@Column(name = "phone_number")
 	private String phoneNumber;
+
 	private String city;
+
 	private String state;
+
+	@Column(name = "zip_code")
 	private String zipCode;
+
 	private String email;
 
 	public ContactData() 
