17518736354c0c1c290ea5a36efa39cb56882018
padmini v <padmini.v@perfios.com>
2021-11-07 23:39:24 +0530

    MODIFIED: Ability to develop rest of crud service methods with mySql database

--- src/main/java/com/bridgelabz/service/AddressBookService.java
+++ src/main/java/com/bridgelabz/service/AddressBookService.java
@@ -1,55 +1,55 @@
 package com.bridgelabz.service;
 
 import java.util.ArrayList;
+
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.bridgelabz.dto.ContactDTO;
 import com.bridgelabz.exception.AddressBookException;
 import com.bridgelabz.model.ContactData;
+import com.bridgelabz.repository.ContactRepository;
 
 @Service
 public class AddressBookService implements IAddressBookService 
 {
-	List<ContactData> contactList = new ArrayList<>();
+	@Autowired
+	private ContactRepository contactRepository;
 
 	@Override
 	public List<ContactData> getContact() 
 	{
-		return contactList;
+		return contactRepository.findAll();
 	}
 
 	@Override
 	public ContactData getContactById(int contactId) 
 	{
-		return contactList.stream().filter(contact -> contact.getContactId() == contactId).findFirst()
-				.orElseThrow(() -> new AddressBookException("Contact not found!"));
+		return contactRepository.findById(contactId).orElseThrow(() -> new AddressBookException("Contact not found!"));
 	}
 
 	@Override
 	public ContactData createContact(ContactDTO contactDTO) 
 	{
-		ContactData contactData = new ContactData(contactList.size()+1, contactDTO);
-		contactList.add(contactData);
-		return contactData;
+		ContactData contactData = new ContactData(contactDTO);
+		return contactRepository.save(contactData);
 	}
 
 	@Override
 	public ContactData updateContact(int contactId, ContactDTO contactDTO) 
 	{
-		contactList.stream().filter(contact -> contact.getContactId() == contactId).findFirst()
+		ContactData contactData = contactRepository.findById(contactId)
 				.orElseThrow(() -> new AddressBookException("Contact not found!"));
-		contactList.remove(contactId - 1);
-		ContactData contactData = new ContactData(contactId, contactDTO);
-		contactList.add(contactId - 1, contactData);
-		return contactData;
+		contactData.updateContactData(contactDTO);
+		return contactRepository.save(contactData);
 	}
 
 	@Override
 	public void deleteContact(int contactId) 
 	{
-		contactList.stream().filter(contact -> contact.getContactId() == contactId).findFirst()
-				.orElseThrow(() -> new AddressBookException("Contact not found!"));
-		contactList.remove(contactId - 1);
+		contactRepository.findById(contactId).orElseThrow(() -> new AddressBookException("Contact not found!"));
+		contactRepository.deleteById(contactId);
 	}
 
 }
