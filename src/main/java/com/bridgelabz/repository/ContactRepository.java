94c48068256dfc2ea2c9e49945c0fb70afb5a294
padmini v <padmini.v@perfios.com>
2021-11-08 23:36:29 +0530

    ADDED: Ability to add queries to find contacts by city,state and zipcode

--- src/main/java/com/bridgelabz/repository/ContactRepository.java
+++ src/main/java/com/bridgelabz/repository/ContactRepository.java
@@ -1,6 +1,9 @@
 package com.bridgelabz.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bridgelabz.model.ContactData;
@@ -8,5 +11,12 @@ import com.bridgelabz.model.ContactData;
 @Repository
 public interface ContactRepository extends JpaRepository<ContactData, Integer>
 {
-
+	@Query(value = "select * from contact where city=:city", nativeQuery = true)
+	List<ContactData> findContactByCity(String city);
+	
+	@Query(value = "select * from contact where state=:state", nativeQuery = true)
+	List<ContactData> findContactByState(String state);
+	
+	@Query(value = "select * from contact where zip_code=:zipCode", nativeQuery = true)
+	List<ContactData> findContactByZipCode(String zipCode);
 }
